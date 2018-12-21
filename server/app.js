c071c0466101fd21574cc902bb05e5b819070e49
Sonia Ruiz <yune.vk@gmail.com>
2018-12-21 11:50:57 +0100

    Fix CORS

--- server/app.js
+++ server/app.js
@@ -78,7 +78,7 @@ require('./passport')(app);
     
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost:3000']
+  origin: ['http://localhost:3000', 'http://tenku.herokuapp.com/']
 }));
 
 const index = require('./routes/index');
